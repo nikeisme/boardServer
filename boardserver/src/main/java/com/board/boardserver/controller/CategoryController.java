361b7a57110c3b67dac340a46f1911c6365d92fd
nikeisme <nikeisme827@gmail.com>
2025-01-06 22:35:03 +0900

    카테고리 controller 어노테이션 수정

--- boardserver/src/main/java/com/board/boardserver/controller/CategoryController.java
+++ boardserver/src/main/java/com/board/boardserver/controller/CategoryController.java
@@ -9,7 +9,9 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
+@RestController
+@RequestMapping("/categories")
+@Log4j2
 public class CategoryController {
 
     private CategoryServiceImpl categoryService;
@@ -18,12 +20,15 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @LoginCheck(type = LoginCheck.UserType.ADMIN)
     public void registerCategory(String accountId, @RequestBody CategoryDTO categoryDTO) {
+
         categoryService.register(accountId, categoryDTO);
     }
+
 
     @PatchMapping("{categoryId}")
     @LoginCheck(type = LoginCheck.UserType.ADMIN)
